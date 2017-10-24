5f79928dad4305a46b57528322c1654b4acb0498
CatcusCat <wjmorris2004@gmail.com>
2017-10-23 17:48:21 -0700

    robotcolorchooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,15 +5,51 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+	Robot jaames= new Robot();
+		
 
 		//3. ask the user what color they would like the robot to draw
-		
+		String w=JOptionPane.showInputDialog("tell me what color do you want(has to be an actual color)");
 		//4. use an if/else statement to set the pen color that the user requested
-
+		  if(w.equals("blue")){
+			  jaames.penDown();
+			  jaames.move(100);
+			  jaames.turn(90);
+		  
+			  
+		  }else if(w.equals("blue")){
+			  jaames.setPenColor(Color.BLUE);
+				for(int i=0;i<4;i++){
+				jaames.penDown();
+			jaames.move (100);
+		jaames.turn(90);
+		  
+				}  
+		  }else if(w.equals("green")){
+			  jaames.setPenColor(Color.green);
+				for(int i=0;i<4;i++){
+				jaames.penDown();
+			jaames.move (100);
+		jaames.turn(90);
+			  
+				 }else if(w.equals("black")){
+					  jaames.setPenColor(Color.black);
+						for(int i=0;i<4;i++){
+						jaames.penDown();
+					jaames.move (100);
+				jaames.turn(90);
+			 
+			  
+			  
+			  
+		  }
         //5. if the user doesn’t enter anything, choose a random color
 
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
@@ -22,6 +58,13 @@ public class RobotColorChooser {
 		
 	    //1. make the robot draw a shape (this will take more than one line of code)
 
-
+		for(int i=0;i<4;i++){
+		jaames.penDown();
+	jaames.move (100);
+jaames.turn(90);
+	
+	
+		}
 	}
+}
 }
